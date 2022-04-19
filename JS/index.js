c0c608cbd1eacf0b2878dce20b76203042f4f41a
David Gonzalo Mercado Juanes <65831379+davejuanes@users.noreply.github.com>
2022-04-19 13:03:26 -0400

    DEF CLASS'S IN JS AND JAVA

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+var car = new Car("AW456", new Account('Dave Mercado', '1234567'))
+
+car.passenger = 4
+car.printDataCar()
--- Java/Main.java
+++ Java/Main.java
@@ -6,17 +6,13 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passenger = 4;
         car.printDataCar();
         // System.out.println("Car License: " + car.license);
 
-        Car car2 = new Car();
+        Car car2 = new Car("EWQ345", new Account("Andrea Herrera", "AND234"));
 
-        car2.license = "EWQ345";
-        car2.driver = "Andrea Herrera";
         car2.passenger = 3;
         // System.out.println("Car License: " + car2.license);
         car2.printDataCar();
