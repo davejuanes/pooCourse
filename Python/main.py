98af5474c6110c29954e341e326951c67c6b2e0b
David Gonzalo Mercado Juanes <65831379+davejuanes@users.noreply.github.com>
2022-04-20 00:41:11 -0400

    ADDED CLASS'S FOR PYTHON LANGUAGE

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,16 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo!")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    car = Car("ASD321", Account('David Mercado', 'DAV432'))
+    # car.license = "AMS234"
+    # car.driver = "Andres Herrera"
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE234"
-    car2.driver = "Andrea Herrera"
+    car2 = Car("REW123", Account('Milay', 'MLY765'))
+    # car2.license = "QWE234"
+    # car2.driver = "Andrea Herrera"
     print(vars(car2))
+    print(vars(car2.driver))
